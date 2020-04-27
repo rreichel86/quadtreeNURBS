1a4f9d632fce25615d69f7009270e2f91beab6fd
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-04-27 22:35:05 +0200

    prealloc NURBS arrar

--- MATLAB_CODE/CalculateNURBS.m
+++ MATLAB_CODE/CalculateNURBS.m
@@ -7,8 +7,11 @@ function NURBS = CalculateNURBS(degree,knots,controlPoints,weights)
 
 counter = 1;%initialize counter
 n=length(controlPoints)-1; %number of knot spans
+subs = knots(1):(knots(end)-knots(1))*0.001:knots(end);
 
-for i=knots(1):(knots(end)-knots(1))*0.001:knots(end)
+NURBS = zeros(3,size(subs,2));
+
+for i = subs
     %loop over parametrical space defined by knot vector
     %obtain each coordinate of the NURBS and store it 
     [f] = curvePoint(n,degree,knots,controlPoints,i,weights);
