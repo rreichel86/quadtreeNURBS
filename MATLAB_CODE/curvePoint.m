3c04e1d0fd5d6576dd37bb6e90fad7d6c0c7a3fc
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-04-27 22:54:52 +0200

    realloc Pw array

--- MATLAB_CODE/curvePoint.m
+++ MATLAB_CODE/curvePoint.m
@@ -2,7 +2,7 @@ function C = curvePoint(n,degree,knots,controlPoints,u,weights)
 %Output: point C corresponding to the "u" parametrical coordinate
 %Input: n,degree,knots,controlPoints,u,weights for the considered NURBS
  
-%Pw = zeros(size(controlPoints,1) + 1, size(controlPoints,2));
+Pw = zeros(size(controlPoints,1) + 1, size(controlPoints,2));
 %Applying perspective map H, defined at the NURBS Book
 for i =1:size(controlPoints, 1)
     Pw(i,:) = controlPoints(i,:) .* weights;
