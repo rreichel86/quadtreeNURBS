30afda08ed8c12055298773c844d51be71875e10
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-07-16 18:32:06 +0200

    improve section for plotting polygonal elements with curves edges

--- MATLAB_CODE/main.m
+++ MATLAB_CODE/main.m
@@ -84,47 +84,93 @@ end
 
 
 %% Plot polygonal elements with curve edges
- for ielno = 3:numel
+ for ielno = 1:numel
      kvno = kv_element{ielno}(2);
      nel = kv_element{ielno}(3);
-     elmt = kv_element{ielno}(4:end);
+     elmt = kv_element{ielno}(4:4+nel-1);
+     scno = kv_element{ielno}(end);
      ecoor = coor( elmt(1:end), 2:3);
+     sc_coor = coor( scno, 2:3);
      wg = coor( elmt(1:end), 4);
      if kvno ~= 0
         nKnot = kv_num{kvno}(5);
         pgrad = kv_num{kvno}(2);
-        knotVector = kv_num{kvno}(6:end)
-        iknot = find( elmt == kv_num{kvno}(3) )
-        eknot = find( elmt == kv_num{kvno}(4) )
+        knotVector = kv_num{kvno}(6:end);
+        iknot = find( elmt == kv_num{kvno}(3) );
+        eknot = find( elmt == kv_num{kvno}(4) );
+        
+        iknot_coor = ecoor(iknot,:);
+        eknot_coor = ecoor(eknot,:);
+        
+        % determine orientation
+        % ori =  1 for CCW
+        % ori = -1 for CW
+        ori = orientation(iknot_coor,eknot_coor,sc_coor);
+        
+        % swap iknot and eknot 
+        if ori == -1 
+            temp = iknot;
+            iknot = eknot;
+            eknot = temp;
+        end
+        
+        
      end    
      
      % plot polygon that dont have curve edges
-     if kvno == 0
-         for ii = 1:nel
-             if ii ~= nel 
-                 a = ii;
-                 b = ii+1;
-             else
-                 a = nel;
-                 b = 1;
-             end    
-             plot(ecoor([a,b],1).', ecoor([a,b],2).', 'r-')
-             hold on 
-         end    
-     end 
+     if kvno == 0 
+%          continue
+%          for ii = 1:nel
+%              if ii ~= nel 
+%                  a = ii;
+%                  b = ii+1;
+%              else
+%                  a = nel;
+%                  b = 1;
+%              end    
+%              plot(ecoor([a,b],1).', ecoor([a,b],2).', 'c-')
+%              
+%              hold on 
+%          end   
+         
+         patch(ecoor(:,1),ecoor(:,2), 'green','FaceAlpha',.5)
+         
+      
 
      % plot polygon that dont have curve edges
-     % ONLY WORKS FOR SOME ELEMENTS !
-     if kvno ~= 0 && iknot < eknot && iknot ~= 1
+     elseif kvno ~= 0 && iknot < eknot && iknot == 1
+%          continue
          ii = 1;
          while ii <= nel 
-             
-             
+            if (ii == eknot) && (iknot == 1)
+                 a = ii;
+                 b = 1;
+                 ii = ii + pgrad - 1;
+                 NURBS_sec=CalculateNURBS(pgrad,knotVector,ecoor([a:nel,1],:)',wg([a:nel,1])');
+                 plot(NURBS_sec(1,:),NURBS_sec(2,:),'b','LineWidth',3);
+             elseif ii ~= nel 
+                 a = ii;
+                 b = ii+1;
+                 plot(ecoor([a,b],1).', ecoor([a,b],2).', 'b-')
+             elseif ii == nel
+                 a = nel;
+                 b = 1;
+                 plot(ecoor([a,b],1).', ecoor([a,b],2).', 'b-')
+             end    
+             hold on 
+             ii = ii + 1;
+         end  
+     
+     
+     elseif kvno ~= 0 && iknot < eknot && iknot ~= 1
+%          continue
+         ii = 1;
+         while ii <= nel 
              if ii == iknot
                  a = ii;
                  b = ii + pgrad;
                  ii = ii + pgrad - 1;
-                 NURBS_sec=CalculateNURBS(pgrad,knotVector,ecoor(a:b,:)',wg(b:-1:a)');
+                 NURBS_sec=CalculateNURBS(pgrad,knotVector,ecoor(a:b,:)',wg(a:b)');
                  plot(NURBS_sec(1,:),NURBS_sec(2,:),'r','LineWidth',3);
              elseif ii ~= nel 
                  a = ii;
@@ -135,15 +181,57 @@ end
                  b = 1;
                  plot(ecoor([a,b],1).', ecoor([a,b],2).', 'r-')
              end    
-               
-             
-             
              hold on 
-             
              ii = ii + 1;
          end    
-     end 
-     
+         
+     elseif kvno ~= 0 && iknot > eknot && eknot == 1
+%          continue
+         ii = 1;
+         while ii <= nel 
+             if (ii == iknot) && (eknot == 1)
+                 a = ii;
+                 b = 1;
+                 ii = ii + pgrad - 1;
+                 NURBS_sec=CalculateNURBS(pgrad,knotVector,ecoor([a:nel,1],:)',wg([a:nel,1])');
+                 plot(NURBS_sec(1,:),NURBS_sec(2,:),'b','LineWidth',3);
+             elseif ii ~= nel 
+                 a = ii;
+                 b = ii+1;
+                 plot(ecoor([a,b],1).', ecoor([a,b],2).', 'b-')
+             elseif ii == nel
+                 a = nel;
+                 b = 1;
+                 plot(ecoor([a,b],1).', ecoor([a,b],2).', 'b-')
+             end    
+             hold on 
+             ii = ii + 1;
+         end  
+         
+     elseif kvno ~= 0 && iknot > eknot && eknot ~= 1
+%          continue
+         ii = 1;
+         while ii <= nel 
+             if ii == eknot
+                 a = ii;
+                  b = ii + pgrad;
+                 ii = ii + pgrad - 1;
+                 NURBS_sec=CalculateNURBS(pgrad,knotVector,ecoor(a:b,:)',wg(a:b)');
+                 plot(NURBS_sec(1,:),NURBS_sec(2,:),'b','LineWidth',3);
+             elseif ii ~= nel 
+                 a = ii;
+                 b = ii+1;
+                 plot(ecoor([a,b],1).', ecoor([a,b],2).', 'b-')
+             elseif ii == nel
+                 a = nel;
+                 b = 1;
+                 plot(ecoor([a,b],1).', ecoor([a,b],2).', 'b-')
+             end    
+             hold on 
+             ii = ii + 1;
+         end    
+             
+     end
      
  end 
 
