68edf6dc94af25ff29416581c26cdf744fb3ee4e
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-07-21 23:51:13 +0200

    move computation and plotting of discrete NURBS points out of this SR

--- MATLAB_CODE/nurbs_brep_quadtree.m
+++ MATLAB_CODE/nurbs_brep_quadtree.m
@@ -1,22 +1,8 @@
 function [Quadtree] = nurbs_brep_quadtree(degree,knots,controlPoints,weights,Boundary)
 
 
-close all;
-clc;
-config;
 
-% CalculateNURBS computes the curve
-NURBS = CalculateNURBS(degree,knots,controlPoints,weights);
-
-% First plotting
-figure(1);
-hold on;
-plot(NURBS(1,:),NURBS(2,:),'r','LineWidth',3);
-plot(controlPoints(1, :), controlPoints(2, :), 'ro','LineWidth',3);
-plot(controlPoints(1, :), controlPoints(2, :), '--','LineWidth',0.5);
-box on
-
-%Count of control points in the root
+% Count of control points in the root
 nPoints = checkQuad( Boundary(:,2)',  Boundary(:,4)',controlPoints);
 
 % Split the root if there is more than one point in the root
