0fed45adbeefb7d92d089da5d8537cc910e29270
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-08-07 00:49:02 +0200

    add description and refactor basisfun.m

--- MATLAB_CODE/basisfun.m
+++ MATLAB_CODE/basisfun.m
@@ -1,22 +1,34 @@
-function N = basisfun(i,u,p,U)                 
-%Adapted from the NURBS Book
-
-i = i + 1; 
-                                                 
-left = zeros(p+1,1);                              
-right = zeros(p+1,1);                            
-                                                
-N(1) = 1;                                         
-for j=1:p                                         
-    left(j+1) = u - U(i+1-j);                     
-    right(j+1) = U(i+j) - u;                      
-    saved = 0;                                    
-    for r=0:j-1                                   
-        temp = N(r+1)/(right(r+2) + left(j-r+1));  
-        N(r+1) = saved + right(r+2)*temp;         
-        saved = left(j-r+1)*temp;                 
-    end                                            
-    N(j+1) = saved;                               
-end                                               
+function N = basisfun(i,u,p,U)
+% function N = basisfun(i,p,u,U)
+% NURBS-Book (algorithm A2.2)
+% evalute nonzero basis functions
+% INPUT:
+% i          : current knotspan
+% u          : evaluation point
+% p          : degree of the basis functions
+% U          : knot vector (row vector)
+% OUTPUT:
+% N          : column vector (dim p+1)
+%              values of the basis function N_(i-p) ... N_(i)
+%              at the evaluation point u
+%
+% --------------------------------------------------------------
+N = zeros(p+1,1);
+N(1) = 1;
+left = zeros(p+1,1);
+right = zeros(p+1,1);
+for j = 1:p
+    left(j+1) = u-U(i+1-j+1);
+    right(j+1) = U(i+j+1)-u;
+    saved = 0;
+    for r = 0:j-1
+        temp = N(r+1)/(right(r+2)+left(j-r+1));
+        N(r+1) = saved + right(r+2)*temp;
+        saved = left(j-r+1)*temp;
+    end
+    N(j+1) = saved;
+end
+end
+                                   
    
                                                   
