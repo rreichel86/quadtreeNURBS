62ed10cef57305f032aebd95f18f45850627bc72
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-08-07 19:04:59 +0200

    refactor Star_Shape SR

--- MATLAB_CODE/Star_Shape.m
+++ MATLAB_CODE/Star_Shape.m
@@ -1,85 +1,66 @@
-function [Quadtree]=Star_Shape(Quadtree, controlPoints, knots, weights, degree, Boundary)
-
-% Star_Shape is the main function which controls the kernel searching.
-% First, it calls starAlgorithm1 for all leaves, that have a piece of the
-% NURBS curve. If necessary, starAlgorithm2 is called. If starAlgorithm2
-% also fails to find kernels for both subdomains, a decomposition is performed 
-% on the quad, for which no kernels can be found is performed
-
-% Input: 
-% Quadtree 
-% data
-% controlPoints 
-% NURBS
-% knots
-% weights
-% degree
+function [Quadtree]=Star_Shape(Quadtree, controlPoints, knots, weights, degree, Boundary, leaves)
+% Star_Shape: Check if Quadtree leaves are star-shaped. Only the Quadtree 
+% leaves that are splitted by the NURBS curve are checked. 
+%
+% INPUT: 
+% Quadtree ------------------- Quadtree data structure
+% controlPoints -------------- NURBS control points 
+% knots ---------------------- NURBS knot vector
+% weights -------------------- NURBS weights
+% degree --------------------- NURBS degree
 % Boundary
+% leaves --------------------- filtered Quadtree leaves (optional)
+%
+% OUTPUT: 
+% Quadtree ------------------- updated Quadtree data structure
+%
+% -------------------------------------------------------------------------
 
-% Output: 
-% Quadtree: Updated Quadtree, containing the coordinates of vertices of the
-% kernels at rows 12 and 13 for each leaf node in Quadtree
+% Find Quadtree leaves
+if ~exist('leaves','var')
+leaves = Quadtree.findleaves();
+end 
 
-% Find the leaves
-l=Quadtree.findleaves();
-flag = 0;
-
-
-% Loop through all leaves in searching for kernels
-for i=1:length(l)
-    
-    % Obtains the intersections of the NURBS in the quad with the quad 
-    intersections=Quadtree.Node{l(i),1}{5,1};
-    % If empty or just 1 intersection, it will skip and won't search for
-    % kernel in that quad
-    if ~isempty(intersections) && length(intersections) ~= 1
-        
-        while true
-            intersections=Quadtree.Node{l(i),1}{5,1};
-            
-            if ~isempty(intersections) && length(intersections) ~= 1
-                % Calls Algorithm1 for kernel searching
-                [Quadtree]=starAlgorithm1(Quadtree,l(i));
-                
-                % Checks if kernels have already been found for that leaf
-                % and breaks if true
-                if ~isempty(Quadtree.Node{l(i),1}{12,1}) && ~isempty(Quadtree.Node{l(i),1}{13,1})
-                    break
-                end
-                % Calls Algorithm2 for kernel searching if the loop wasn't
-                % broken
-                [Quadtree]=starAlgorithm2(Quadtree,l(i));
-                
-                % Checks if kernels have already been found for that leaf
-                % and breaks if true
-                if ~isempty(Quadtree.Node{l(i),1}{12,1}) && ~isempty(Quadtree.Node{l(i),1}{13,1})
-                    break
-                end
-                
-                % If the loop still hasn't been broken, no kernel were
-                % found after Algorithm2 , so a decomposition is needed
-                [Quadtree]=Decompose_Star(Quadtree, controlPoints, ...
-                    knots, weights, degree,l(i),Boundary);
-                flag = 1;
-                break
-                
-            else
-                
-                break
-                
-            end
-            
+% Filter Quadtree leaves
+for i = 1:length(leaves)
+    intersections=Quadtree.Node{leaves(i),1}{5,1};
+    % Quadtree leaves with 0 or 1 intersection
+    if isempty(intersections) || length(intersections) == 1
+        leaves(i) = 0;      
+    else  % Quadtree leaves with 2 intersections 
+        % use algorithm 1
+        % check if the 2 Quad subregions are star-shaped
+        [Quadtree] = starAlgorithm1(Quadtree,leaves(i));
+        if ~isempty(Quadtree.Node{leaves(i),1}{12,1}) && ~isempty(Quadtree.Node{leaves(i),1}{13,1})
+            leaves(i) = 0; % delete Quad from the list
+            continue
         end
-        
-        if flag == 1
-            % If the quad has been decomposed, the kernel searching must
-            % start from the beginning for the new situation 
-            Star_Shape(Quadtree,controlPoints, knots, weights, degree, Boundary);
-            break;
-            
-        end
+        % if algorithm 1 fails use algorithm 2
+        % check if the 2 Quad subregions are star-shaped
+        [Quadtree] = starAlgorithm2(Quadtree,leaves(i));
+        if ~isempty(Quadtree.Node{leaves(i),1}{12,1}) && ~isempty(Quadtree.Node{leaves(i),1}{13,1})
+            leaves(i) = 0; % delete Quad from the list
+            continue
+        end 
     end
-    
+end
+% update leaves 
+leaves = leaves(leaves~=0);
+
+if isempty(leaves)
+    return
+end     
+
+% subdivide remainig leaves
+newLeaves = zeros(1, 4*length(leaves));
+for i = 1:length(leaves)
+    [Quadtree] = Decompose_Star(Quadtree, controlPoints, ...
+    knots, weights, degree,leaves(i),Boundary);
+    % get children pointers of current Quad 
+    newLeaves(4*i-3:4*i) = Quadtree.Node{leaves(i),1}{11,1};
 end
 
-end
+% chech if new Quadtree leaves are shar-shaped
+[Quadtree] = Star_Shape(Quadtree,controlPoints, knots, weights, degree, Boundary, newLeaves);
+
+
