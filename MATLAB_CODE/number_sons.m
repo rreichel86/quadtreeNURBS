39e183c7c5726493cc068fddbbf1aaf4dbb1e2e1
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-08-11 17:30:39 +0200

    refactor number_sons

--- MATLAB_CODE/number_sons.m
+++ MATLAB_CODE/number_sons.m
@@ -1,31 +1,19 @@
-function [nSons]= number_sons(Location,Loc_Current)
+function [nSons]= number_sons(refList,currentRef)
 % number_sons function takes as input the address book of all the nodes and
 % the position of the actual quad and checks how many levels of
 % decompoition it has, by comparing its coordinates with the addresses of
 % the book, which have the same beggining. Gives as output a scalar nSons
 % beeing it the number of levels of decomposition
 
-%finds elements with at least same length
-idx = cellfun('length',Location)>=length(Loc_Current);
-out = cell(length(Location),1);
-for j=1:length(idx)
-    if idx(j)==1
-        %finds elements with the same depth as Loc_Current
-        out{j}=Location{j}(1:length(Loc_Current));
-    end
-end
-k=1;
-for j=1:length(out)
-    tf=isequal(out{j},Loc_Current);
-    %finds the indexes of the descendents
-    if tf;idx2(k)=j;
-        k=k+1;
-    end
-end
-Sons=cell(length(idx2),1);
-%creates a cell with the descendents
-for j=1:length(idx2);Sons{j}=Location{idx2(j)};end
-%check how many desdendents
-[s,d] = cellfun(@size,Sons);
-nSons= max([d])-length(Loc_Current);
+% current Quad depth
+depth = length(currentRef);
+% get Quad, that are at least at the same depth
+idx = cellfun('length',refList) >= depth;
+filterRefList = refList(idx);
+idx = cellfun(@(x) isequal(x(1:depth),currentRef),filterRefList);
+% list descendents 
+descendents = filterRefList(idx);
+% check how many descendents
+[~,d] = cellfun(@size,descendents);
+nSons = max(d)-depth;
 end
