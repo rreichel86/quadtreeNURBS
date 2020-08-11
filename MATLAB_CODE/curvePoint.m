8654b9239884f9a26e8fb956f72695e0a5ef3010
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-08-11 17:03:48 +0200

    compute dim and number of control points and rewrite prealloc of Pw matrix

--- MATLAB_CODE/curvePoint.m
+++ MATLAB_CODE/curvePoint.m
@@ -2,9 +2,11 @@ function C = curvePoint(n,degree,knots,controlPoints,u,weights)
 %Output: point C corresponding to the "u" parametrical coordinate
 %Input: n,degree,knots,controlPoints,u,weights for the considered NURBS
  
-Pw = zeros(size(controlPoints,1) + 1, size(controlPoints,2));
+ndim = size(controlPoints,1);
+nControlPoints = size(controlPoints,2);
+Pw = zeros(ndim + 1, nControlPoints);
 %Applying perspective map H, defined at the NURBS Book
-for i =1:size(controlPoints, 1)
+for i = 1 : ndim
     Pw(i,:) = controlPoints(i,:) .* weights;
 end
 Pw(3,:) = weights;
