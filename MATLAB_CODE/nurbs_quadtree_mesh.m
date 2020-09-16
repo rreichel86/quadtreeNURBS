7f0dd307f144143d22767083d005c74ba636f38c
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-09-16 12:02:08 +0200

    describe output arguments

--- MATLAB_CODE/nurbs_quadtree_mesh.m
+++ MATLAB_CODE/nurbs_quadtree_mesh.m
@@ -1,5 +1,31 @@
 function [nnode,coor,numsec,maxnsec,sections,ord,knots,wgt] = nurbs_quadtree_mesh(k_min,degree,knots,controlPoints,weights,Boundary)
 
+% OUTPUT:
+% nnode -------------------- number of coordinates = number of nodes
+% coor ----------------------- nodes coordinates and weights 
+% coor = [number, x-coor, y-coor, weight, type, which_region, inside_region]
+%
+%                              type: 1 -  node
+%                                    2 - control point or intersection point
+%                              which_region: region number
+%                              inside_region: 0 - at the boundary
+%                                             1 - inside 
+%                                            -1 - outside
+%
+% numsec ---------------------- number of sections
+% maxnsec --------------------- maximum number of nodes on any section
+% sections -------------------- sectionsconnectivity matrix as nsec-tupel of 
+%                               nodes, where the first three entries
+%                               isec - section number
+%                               ikv - knot vector number
+%                               nsec - number of nodes per section
+% sections = [isec, ikv, nsec, node_1,...,node_nsec]
+% 
+% ord ------------------------- section polynomial order
+% ord = [isec, pgrad, qgrad]
+%
+% knots = [ikv, iw, nknots, iknot, jknot, knot_1,...,knot_nknots]
+% wgt = [iw, nweights, weight_1,...,weigth_nweigths]
 
 
 %% Quadtree decomposition
