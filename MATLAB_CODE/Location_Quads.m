3cb6bbb15f1902788807543e1092a8c8febe5d34
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-10-04 11:06:17 +0200

    refactor code snippet

--- MATLAB_CODE/Location_Quads.m
+++ MATLAB_CODE/Location_Quads.m
@@ -7,10 +7,15 @@ for i=2:l
     Current=Quadtree.Node{i,1}{2};
     for j=1:length(Current)/2
         aux=[Current(2*(j-1)+1) Current(2*(j-1)+2)];
-        if aux==[1 1];Location{i}=[Location{i} 1];end
-        if aux==[2 1];Location{i}=[Location{i} 2];end
-        if aux==[1 2];Location{i}=[Location{i} 3];end
-        if aux==[2 2];Location{i}=[Location{i} 4];end                  
+        if isequal(aux,[1 1])
+            Location{i}=[Location{i} 1];
+        elseif isequal(aux,[2 1])
+            Location{i}=[Location{i} 2];
+        elseif isequal(aux,[1 2])
+            Location{i}=[Location{i} 3];
+        elseif isequal(aux,[2 2])
+            Location{i}=[Location{i} 4];
+        end                  
     end    
 end
 end
