07d46b4a6395a641664766e01d1a1b33865ee458
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-10-08 10:28:24 +0200

    refactor Inter.m

--- MATLAB_CODE/Inter.m
+++ MATLAB_CODE/Inter.m
@@ -13,91 +13,122 @@ function [Pint,U] = Inter(x1,y1,x2,y2,degree,knots,controlPoints,weights)
 % U: paramrametrical coordinates of the intersection point(empty if none)
 
 % Initialization of variables
-Pint=[];
-U=[];
-index=[];
+Pint = [];
+U = [];
 tol = 1e-10;
-jk=[];
-index=[];
-num_jk=0;
 
+intrsc_1 = [];
+num_intrsc_1 = 0;
+intrsc_2 = [];
+num_intrsc_2 = 0;
+a_array = [];
+num_a = 0;
 
 % number of control points / basis functions - 1
 n = length(knots)-degree-2;
 % ncp number of control points 
 ncp = size(controlPoints,2);
 
-% First we obtain if there is an intersection between an control poligon
-% segment and the quad's edge
-for i = 1:ncp-1
-    % loop over the control points. Each segment of the control poligon is
-    % defined by two consecutive control points.
-    Px = controlPoints(1,i);
-    Py = controlPoints(2,i);
-    if i ~= ncp 
-        Qx = controlPoints(1,i+1);
-        Qy = controlPoints(2,i+1);
-    end
-    %Checks 4 orientations between Pi,Qi,A,B
-    OI = orientation([x1 y1],[x2 y2],[Px,Py]);
-    OII = orientation([x1 y1],[x2 y2],[Qx,Qy]);
-    OIII = orientation([Px,Py],[Qx,Qy],[x1 y1]);
-    OIV = orientation([Px,Py],[Qx,Qy],[x2 y2]);
-    % If the condition is fulfilled there is a intersection. Obtain the
-    % indexes of the control point array that defines the intersecting
-    % segement and the number of intersections.
-    if OI ~= OII && OIII ~= OIV        
-        if OI ~= 0
-            index = [index i i+1];
-        elseif OI == 0
-            if i == 1
-                index = [index i i+1];
-            else
-                index =[index i+1];
-            end
-        end
-    end    
-end
-num_inter = length(index)/2;
-% Correcting quad's segment coordinates
+% First we obtain if there is an intersection between a control polygon
+% and the quad's edge
+
+% a vertical edge
 if abs(x2 - x1) < tol
-    y1 = y1 + aux;
-    y2 = y2 - aux;
+    coorVal = x1;
+    coorIdx = 1;
+    coorInterval = [y1, y2];
+    
+% a horizontal edge    
 elseif abs(y2 - y1) < tol
-    x1 = x1 + aux;
-    x2 = x2 - aux;
+    coorVal = y1;
+    coorIdx = 2;
+    coorInterval = [x1, x2];
 end
 
-% Next step is to obtain the first aproximation of the solution
-for i = 1:num_inter
-    % We loop over the segments of the control poligon that intersect the
-    % quad's edge
-    if abs(x2 - x1) < tol
-        coorVal = x1;
-        coorIdx = 1;
+% Loop over the control points
+for i = 1:ncp-1
+   
+    P = controlPoints(:,i);
+    
+    if i ~= ncp 
+        Q = controlPoints(:,i+1);
+    end 
+    
+    dP = ( P(coorIdx) - coorVal );
+    dQ = ( Q(coorIdx) - coorVal );
+    
+    if dP * dQ < 0 
+       intrsc_2 = [intrsc_2 i i+1];
+    elseif abs(dP) < tol   
+       intrsc_1 =  [intrsc_1 i];
+    elseif  abs(dQ) < tol
+       intrsc_1 =  [intrsc_1 i+1];
+    end     
+    
         
-    elseif abs(y2 - y1) < tol
-        coorVal = y1;
-        coorIdx = 2;
-        
-    end
+end     
+
+
+if isempty(intrsc_2) && isempty(intrsc_1)
+    return
+elseif  ~isempty(intrsc_2)
+    
+    num_intrsc_2 = length(intrsc_2)/2;
+    
+elseif  ~isempty(intrsc_1)  
+    
+    intrsc_1 = unique(intrsc_1,'stable');
+    num_intrsc_1 = length(intrsc_1);
+    
+end 
+
+% Next step is to obtain an approximation of the solution 
+for i = 1:num_intrsc_2
+   
     % findIntrscApprox scans the part of the NURBS controlled by the control
     % points that define the intersected segment. It obtains the first
-    % approximation of the solution as output (jj)
-    [jj,flag] = findIntrscApprox(coorVal,coorIdx, index((2*i-1):2*i),degree,knots,...
-        controlPoints,weights,x1,x2,y1,y2,jk);
+    % approximation of the solution as output (a_pt)
+    
+    [a_pt,flag] = findIntrscApprox(coorVal,coorIdx,intrsc_2((2*i-1):2*i),degree,knots,...
+        controlPoints,weights,x1,x2,y1,y2,a_array);
     if flag == 1
-        num_jk = num_jk + 1;
-        jk = [jk jj];
+        num_a = num_a + 1;
+        a_array = [a_array a_pt];
     end
+    
 end
+
+for i = 1:num_intrsc_1
+   
+    % findIntrscApprox scans the part of the NURBS controlled by the control
+    % points that define the intersected segment. It obtains the first
+    % approximation of the solution as output (a_pt)
+    
+    [a_pt,flag] = findIntrscApprox(coorVal,coorIdx,intrsc_1(i:i),degree,knots,...
+        controlPoints,weights,x1,x2,y1,y2,a_array);
+    if flag == 1
+        num_a = num_a + 1;
+        a_array = [a_array a_pt];
+    end
+    
+end
+
+if (num_a == 0) 
+    return
+end    
+
+a_knots_array = a_array(3,:);
+% avoid duplicated start values
+a_knots_array = unique(a_knots_array);
+num_a = length(a_knots_array);
+
 
 hold on;
 % Based on Steffensen's method obtains the closest solution for a
 % given tolerance
-for i=1:num_jk
+for i=1:num_a
     %loops over the approximated solutions
-    a = jk(i);
+    a = a_knots_array(i);
     hh = 1e-2;
     for ii=1:20
         [f] = curvePoint(n,degree,knots,controlPoints,a,weights);
@@ -106,13 +137,23 @@ for i=1:num_jk
         if err < tol
             %Avoiding to obtain multiplicities,
             if any(abs(U-a)>tol) || isempty(U)
-                if coorIdx==1 && f(2,1)>y1 && f(2,1)<y2
-                    Pint=[Pint f(:)];
-                    U=[U a];
-                end
-                if coorIdx==2 && f(1,1)>x1 && f(1,1)<x2
-                    Pint=[Pint f(:)];
-                    U=[U a];
+                if coorIdx == 1 
+                    if f(2) > y1 && f(2) < y2
+                        Pint = [Pint f(:)];
+                        U = [U a];
+                    elseif ( abs( f(2) - y1 ) < tol ) ||  ( abs( f(2) - y2 ) < tol )
+                        Pint = [Pint f(:)];
+                        U = [U a];
+                    end 
+                
+                elseif coorIdx == 2
+                    if f(1) > x1 && f(1) < x2
+                        Pint = [Pint f(:)];
+                        U = [U a];
+                    elseif ( abs( f(1) - x1 ) < tol ) ||  ( abs( f(1) - x2 ) < tol )
+                        Pint = [Pint f(:)];
+                        U = [U a];
+                    end    
                 end
             end
             break
@@ -130,13 +171,23 @@ for i=1:num_jk
         if ii==20
             %Avoiding to obtain multiplicities
             if any(abs(U-a)>tol) || isempty(U)
-                if coorIdx==1 && f(2,1)>y1 && f(2,1)<y2
-                    Pint=[Pint f(:)];
-                    U=[U a];
-                end
-                if coorIdx==2 && f(1,1)>x1 && f(1,1)<x2
-                    Pint=[Pint f(:)];
-                    U=[U a];
+                if coorIdx == 1 
+                    if f(2) > y1 && f(2) < y2
+                        Pint = [Pint f(:)];
+                        U = [U a];
+                    elseif ( abs( f(2) - y1 ) < tol ) ||  ( abs( f(2) - y2 ) < tol )
+                        Pint = [Pint f(:)];
+                        U = [U a];
+                    end 
+                
+                elseif coorIdx == 2
+                    if f(1) > x1 && f(1) < x2
+                        Pint = [Pint f(:)];
+                        U = [U a];
+                    elseif ( abs( f(1) - x1 ) < tol ) ||  ( abs( f(1) - x2 ) < tol )
+                        Pint = [Pint f(:)];
+                        U = [U a];
+                    end    
                 end
             end
         end
