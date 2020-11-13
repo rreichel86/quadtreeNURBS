65eb403fe4770f21812f38c666277e96ae367265
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-11-13 11:45:13 +0100

    decrease bounding box size

--- MATLAB_CODE/NURBS_parameters.m
+++ MATLAB_CODE/NURBS_parameters.m
@@ -15,8 +15,8 @@ if flag==2
                    -1 -1 0 1 1 1 0 -1 -1];
     knots=[0 0 0 0.25 0.25 0.5 0.5 0.75 0.75 1 1 1];  
     weights=[1 0.707 1 0.707 1 0.707 1 0.707 1];
-    Boundary=[-5 -5 5 5;...
-              5 -5 -5 5];
+    Boundary=[-2.5 -2.5 2.5 2.5;...
+              2.5 -2.5 -2.5 2.5];
     axis=([min(controlPoints(1,:))*1.2, max(controlPoints(1,:))*1.2,...
         min(controlPoints(2,:))*1.2, max(controlPoints(2,:))*1.2,]);
 end
