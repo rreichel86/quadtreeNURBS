c918c0f15cd04404dfb2f702bffc644203623add
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-11-25 00:48:57 +0100

    rename vars

--- MATLAB_CODE/plot_leaf.m
+++ MATLAB_CODE/plot_leaf.m
@@ -1,20 +1,22 @@
-l=Quadtree.findleaves();
 function plot_leaf(Quadtree)
 % plot_leaf: plots each quadtree's leaf
 
 % Get Quadtree leaves
+leaves = Quadtree.findleaves();
+numLeaves = length(leaves);
 
 figure(3)
 axis square
 hold on
-for i=1:length(l)
+
+for i = 1:numLeaves
     
-    intersections=Quadtree.Node{l(i),1}{5,1};
+    intersections=Quadtree.Node{leaves(i),1}{5,1};
     
     % check if leaf has intersections
     if isempty(intersections) || length(intersections) == 1
-         plot(Quadtree.Node{l(i),1}{10,1}(1,:),Quadtree.Node{l(i),1}{10,1}(2,:),'k.-');
         % Plot Quad
+        plot(Quadtree.Node{leaves(i),1}{10,1}(1,:),Quadtree.Node{leaves(i),1}{10,1}(2,:),'k.-');
     else
         % Obtain NURBS definition of the curve contained in each leaf
         NURBS.degree = Quadtree.Node{leaves(i),1}{6,1};
@@ -26,14 +28,16 @@ for i=1:length(l)
         plot(NURBS_pts(:,1),NURBS_pts(:,2),'r','LineWidth',2.5);
         
         % Plot Quad
+        plot(Quadtree.Node{leaves(i),1}{10,1}(1,:),Quadtree.Node{leaves(i),1}{10,1}(2,:),'k.-');
         
-        plot(Quadtree.Node{l(i),1}{10,1}(1,:),Quadtree.Node{l(i),1}{10,1}(2,:),'k.-');
-        plot(Quadtree.Node{l(i),1}{7,1}(1,:),Quadtree.Node{l(i),1}{7,1}(2,:),'b-.','LineWidth',1)
-        plot(Quadtree.Node{l(i),1}{7,1}(1,:),Quadtree.Node{l(i),1}{7,1}(2,:),'bo','LineWidth',1.5)
         % Plot control points and control polygon for each leaf
+        plot(NURBS.controlPoints(1,:),NURBS.controlPoints(2,:),'b-.','LineWidth',1)
+        plot(NURBS.controlPoints(1,:),NURBS.controlPoints(2,:),'bo','LineWidth',1.5)
         
     end
 end
+hold off
+
 end
 
 
