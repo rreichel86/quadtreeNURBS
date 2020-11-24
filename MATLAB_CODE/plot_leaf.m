c704acdaa911d7c687a7844c02f05849f5ec333d
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-11-25 00:48:17 +0100

    improve comments

--- MATLAB_CODE/plot_leaf.m
+++ MATLAB_CODE/plot_leaf.m
@@ -2,19 +2,20 @@ l=Quadtree.findleaves();
 function plot_leaf(Quadtree)
 % plot_leaf: plots each quadtree's leaf
 
+% Get Quadtree leaves
+
 figure(3)
 axis square
 hold on
 for i=1:length(l)
     
     intersections=Quadtree.Node{l(i),1}{5,1};
-    % if empty plot empty quadrant
+    
+    % check if leaf has intersections
     if isempty(intersections) || length(intersections) == 1
          plot(Quadtree.Node{l(i),1}{10,1}(1,:),Quadtree.Node{l(i),1}{10,1}(2,:),'k.-');
+        % Plot Quad
     else
-        % if each leaf may be plotted separately, uncomment
-        %figure(i+1)
-        
         % Obtain NURBS definition of the curve contained in each leaf
         NURBS.degree = Quadtree.Node{leaves(i),1}{6,1};
         NURBS.knots  = Quadtree.Node{leaves(i),1}{8,1};
@@ -24,16 +25,12 @@ for i=1:length(l)
         NURBS_pts = CalculateNURBS(NURBS);
         plot(NURBS_pts(:,1),NURBS_pts(:,2),'r','LineWidth',2.5);
         
-        % setting axis from function NURBS_parameter
-%         axis(ax);
+        % Plot Quad
         
-        
-        % Plotting  leaf's quad
         plot(Quadtree.Node{l(i),1}{10,1}(1,:),Quadtree.Node{l(i),1}{10,1}(2,:),'k.-');
-        
-        % Plotting control points and control polygon for each leaf
         plot(Quadtree.Node{l(i),1}{7,1}(1,:),Quadtree.Node{l(i),1}{7,1}(2,:),'b-.','LineWidth',1)
         plot(Quadtree.Node{l(i),1}{7,1}(1,:),Quadtree.Node{l(i),1}{7,1}(2,:),'bo','LineWidth',1.5)
+        % Plot control points and control polygon for each leaf
         
     end
 end
