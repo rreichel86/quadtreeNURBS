9e2d3d915132b879b79616a4a0aa6647dbb6830d
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-11-27 18:58:01 +0100

    change dim locQ array

--- MATLAB_CODE/ref2loc.m
+++ MATLAB_CODE/ref2loc.m
@@ -2,7 +2,7 @@ function locQ = ref2loc(refQ)
 % ref2loc: compute Quad location in 1:4 format (11 = 1, 21 = 2, 12 = 3, 22 = 4)
 
 lenlocQ = length(refQ)/2;
-locQ = zeros(lenlocQ,1);
+locQ = zeros(1,lenlocQ);
 
 for l = 1:lenlocQ
     
