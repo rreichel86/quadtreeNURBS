05c9a15bd56d655d67a291de6bfb63fc4265b35c
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-12-20 21:50:34 +0100

    update plot_leaf SR

--- MATLAB_CODE/plot_leaf.m
+++ MATLAB_CODE/plot_leaf.m
@@ -24,15 +24,19 @@ for i = 1:numLeaves
         NURBS.controlPoints = Quadtree.Node{leaves(i),1}{7,1};
         NURBS.weights = Quadtree.Node{leaves(i),1}{9,1};
        
+        ncp = length(NURBS.knots) - NURBS.degree - 1;
+        
         NURBS_pts = CalculateNURBS(NURBS);
-        plot(NURBS_pts(:,1),NURBS_pts(:,2),'r','LineWidth',2.5);
+        plot(NURBS_pts(:,1),NURBS_pts(:,2),'r','LineWidth',2);
         
         % Plot Quad
         plot(Quadtree.Node{leaves(i),1}{10,1}(1,:),Quadtree.Node{leaves(i),1}{10,1}(2,:),'k.-');
         
         % Plot control points and control polygon for each leaf
         plot(NURBS.controlPoints(1,:),NURBS.controlPoints(2,:),'b-.','LineWidth',1)
-        plot(NURBS.controlPoints(1,:),NURBS.controlPoints(2,:),'bo','LineWidth',1.5)
+        plot(NURBS.controlPoints(1,1),NURBS.controlPoints(2,1),'bo','LineWidth',1.5)
+        plot(NURBS.controlPoints(1,2:ncp-1),NURBS.controlPoints(2,2:ncp-1),'o','Color','red','MarkerFaceColor','r','MarkerSize',6)
+        plot(NURBS.controlPoints(1,ncp),NURBS.controlPoints(2,ncp),'bo','LineWidth',1.5)
         
     end
 end
