434086a6c681a3204aa14cbbe5a37be73618a28c
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-12-21 09:12:15 +0100

    improve deleted comments and code lines

--- MATLAB_CODE/savetree.m
+++ MATLAB_CODE/savetree.m
@@ -79,10 +79,7 @@ if k == 1
     data{2} = [data{2} nodeID];
     Quadtree = Quadtree.set(1, data);
 else
-    % Storing data for any other level of decomposition
     for j=1:length(newKnots)/2
-        % Loop over the inserted knots and selecting section of the NURBS
-        % contained in the quad
         k0 = find( abs(knots_new - newKnots(2*(j-1)+1)) < 1e-10, 1, 'First');
         k1 = find( abs(knots_new - newKnots(2*(j-1)+2)) < 1e-10, 1, 'Last');
         
@@ -106,8 +103,6 @@ else
         end
     end
     
-    % Store information at tree's new node 
-    % and attach it to its father
     
     % Quad name 
     % Quad location
@@ -131,11 +126,9 @@ else
             weights2;...
             QS;...
             []};
-       
 %     for i = 1:length(Quadtree.Node)
 %         Pos{i} = Quadtree.Node{i,1}{2};
 %     end
-      Q_aux = Q_aux(1:end-2);
 %     idx = cellfun('length',Pos) == length(Q_aux);
 %     for j = 1:length(idx)
 %         tf = isequal(Pos{j},Q_aux);
@@ -143,13 +136,7 @@ else
 %             Parent = j;
 %         end
 %     end
-    
-    Parent = findQ(Quadtree,Q_aux);
-    [Quadtree, node2] = Quadtree.addnode(Parent, data);
-    
-    % Adding pointer to father's new child
     data = Quadtree.Node{Parent,1};
-    data{11} = [data{11} node2];
     Quadtree = Quadtree.set(Parent, data);
     
     % Deleting information of the father, information contained in leafs
