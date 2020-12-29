536f90610a41bc799e25ed51b19bb38132d6b0e9
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2020-12-29 16:47:36 +0100

    move deleted code lines to extractNURBS_segment SR

--- MATLAB_CODE/savetree.m
+++ MATLAB_CODE/savetree.m
@@ -14,46 +14,7 @@ function [Quadtree] = savetree(Q_aux, Quadtree,k, Px, Py, knotVals,...
 % Output:
 % Quadtree after storing new information
 
-% NURBS segment
-NURBS_segment = struct;
 
-if ~isempty(NURBS)
-    
-    degree = NURBS.degree;
-    knots = NURBS.knots;
-    controlPoints = NURBS.controlPoints;
-    weights = NURBS.weights;
-    
-    % Loop over the inserted knots and extact NURBS segment
-    % contained in current quad
-    for j = 1:length(knotVals)/2
-        
-        k0 = find( abs(knots - knotVals(2*(j-1)+1)) < 1e-10, 1, 'First');
-        k1 = find( abs(knots - knotVals(2*(j-1)+2)) < 1e-10, 1, 'Last');
-        
-        if k0 == 1 && k1 ~= length(knots)
-            NURBS_segment.degree = degree;
-            NURBS_segment.controlPoints = controlPoints(1:2,k0:(k1-degree));
-            NURBS_segment.knots = [knots(k0:k1) knots(k1)];
-            NURBS_segment.weights = weights(k0:(k1-degree));
-        elseif k0 ~= 1 && k1 == length(knots)
-            NURBS_segment.degree = degree;
-            NURBS_segment.controlPoints = controlPoints(1:2,(k0-1):(k1-degree-1));
-            NURBS_segment.knots = [knots(k0) knots(k0:k1)];
-            NURBS_segment.weights = weights((k0-1):(k1-degree-1));
-        elseif k0 ~= 1 && k1 ~= length(knots)
-            NURBS_segment.degree = degree;
-            NURBS_segment.controlPoints = controlPoints(1:2,(k0-1):(k1-degree));
-            NURBS_segment.knots = [knots(k0) knots(k0:k1) knots(k1)];
-            NURBS_segment.weights = weights((k0-1):(k1-degree));
-        else
-            NURBS_segment.degree = degree;
-            NURBS_segment.controlPoints = controlPoints;
-            NURBS_segment.knots = knots;
-            NURBS_segment.weights = weights;
-        end
-    end
-end
 % Store information in tree's new node
 
 % 1. Quad name
