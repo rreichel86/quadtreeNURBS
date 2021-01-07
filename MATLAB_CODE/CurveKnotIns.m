3b31e01aa4c47013fe451652f0f1d4569b4e49ed
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2021-01-07 12:40:29 +0100

    rewrite loops

--- MATLAB_CODE/CurveKnotIns.m
+++ MATLAB_CODE/CurveKnotIns.m
@@ -24,59 +24,39 @@ for i = k+1:mp
     knots_new(i+r) = knots(i);
 end
 
+% Save unaltered control points 
 for i = 1:k-p
-    Qw(1,i) = Pw(1,i);
-    Qw(2,i) = Pw(2,i);
-    Qw(3,i) = Pw(3,i);
-    
+    Qw(:,i) = Pw(:,i);
 end
 
 for i = k-s:np
-    Qw(1,i+r) = Pw(1,i);
-    Qw(2,i+r) = Pw(2,i);
-    Qw(3,i+r) = Pw(3,i);
+    Qw(:,i+r) = Pw(:,i);
 end
 
 for i = 1:p-s+1
-    Rw(1,i) = Pw(1,k-p+i-1);
-    Rw(2,i) = Pw(2,k-p+i-1);
-    Rw(3,i) = Pw(3,k-p+i-1);
-    
+    Rw(:,i) = Pw(:,k-p+i-1);
 end
 
+% Insert the knot r times
 for j = 1:r
     L = k-p+j;
     
-    
     for i = 1:p-j-s+1
-        alpha = (newKnot- knots(L+i-1))/(knots(i+k) - knots(L+i-1));
-        Rw(1,i) = alpha*Rw(1,i+1) + (1.0-alpha)*Rw(1,i);
-        Rw(2,i) = alpha*Rw(2,i+1) + (1.0-alpha)*Rw(2,i);
-        Rw(3,i) = alpha*Rw(3,i+1) + (1.0-alpha)*Rw(3,i);
+        alpha = (newKnot - knots(L+i-1))/(knots(i+k) - knots(L+i-1));
+        Rw(:,i) = alpha*Rw(:,i+1) + (1.0-alpha)*Rw(:,i);
     end
     
-    Qw(1,L) = Rw(1,1);
-    Qw(2,L) = Rw(2,1);
-    Qw(3,L) = Rw(3,1);
-    Qw(1,k+r-j-s) = Rw(1,p-j-s+1);
-    Qw(2,k+r-j-s) = Rw(2,p-j-s+1);
-    Qw(3,k+r-j-s) = Rw(3,p-j-s+1);
+    Qw(:,L) = Rw(:,1);
+    Qw(:,k+r-j-s) = Rw(:,p-j-s+1);
 end
 
+% Load remaining control points
 for i = (L+1):(k-s-1)
-    Qw(1,i) = Rw(1,i-L+1);
-    Qw(2,i) = Rw(2,i-L+1);
-    Qw(3,i) = Rw(3,i-L+1);
+    Qw(:,i) = Rw(:,i-L+1);
 end
 
-for i = 1:length(Qw)
-    Q(1,i) = Qw(1,i)/ Qw(3,i);
-    Q(2,i) = Qw(2,i)/ Qw(3,i);
-    weights(i) =  Qw(3,i);
-end
-
-
-
+Q = Qw(1:2,:) ./ Qw(3,:);
+weights = Qw(3,:);
 
 end
 
