729b7bbec4c82cc53fa58de53dea818909e68e3d
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2021-01-11 16:07:50 +0100

    get data stored current leaf

--- MATLAB_CODE/starAlgorithm1.m
+++ MATLAB_CODE/starAlgorithm1.m
@@ -16,8 +16,34 @@ function [star_shaped, Quadtree] = starAlgorithm1(Quadtree,idx)
 
 tol = 1e-10;
 
+% get data stored in current leaf
 
+% 1. Quad name
+% 2. Quad location
+% 3. intersections points (physical space)
+% 4.
+% 5. intersection in parametric space of the curve
+% 6. NURBS definition
+%    NURBS degree
+%    NURBS control points
+%    NURBS Knot vector
+%    NURBS weights
+% 7. Quad definition
+% 8. Pointer to the children
 
+data = Quadtree.Node{idx,1};
+
+% quad vertices 
+quadVertices = data{7};
+% number of quad vertices
+numQuadVertices = size(quadVertices,2);
+% control points
+NURBS_segment = data{6};
+controlPoints = NURBS_segment.controlPoints; 
+% intersecion points
+intersectionPoints = data{3}; 
+% number of intersection points
+numIntersectionPoints = size(intersectionPoints,2);
     end
     end
 end
