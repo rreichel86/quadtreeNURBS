9aea0b5888d33ca50b1d1e6723b067baab79d83c
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2021-01-11 16:04:44 +0100

    refactor deleted code lines

--- MATLAB_CODE/starAlgorithm1.m
+++ MATLAB_CODE/starAlgorithm1.m
@@ -15,62 +15,13 @@ function [star_shaped, Quadtree] = starAlgorithm1(Quadtree,idx)
 % -------------------------------------------------------------------------
 
 tol = 1e-10;
-% Quad vertices
-vertices = Quadtree.Node{idx,1}{7,1}(:,1:end)';
-% control points
-controlPoints = Quadtree.Node{idx,1}{6,1}.controlPoints';
-% vertical intersections [left; right] 
-vertIntrsc = Quadtree.Node{idx,1}{4,1}';
-nVertIntrsc = size(vertIntrsc,1);
-% horizontal intersections [bottom; top]
-horzIntrsc = Quadtree.Node{idx,1}{3,1}';
-nHorzIntrsc = size(horzIntrsc,1);
-% intersections
-intrsc = [horzIntrsc;vertIntrsc];
 
-% Quad left corner
-Qxmin = min(vertices(:,1));
-Qymin = min(vertices(:,2));
 
-% intersection points indices
-iintrsc = zeros(2,1);
-poly = zeros(9,2) - 99;
-poly(1:2:8,:) = vertices;
-poly(9,:) = vertices(1,:);
 
-if nVertIntrsc == 2
-    poly(4,:) = intrsc(2,1:2);
-    poly(8,:) = intrsc(1,1:2); 
-    iintrsc = [4,8];
-elseif nHorzIntrsc == 2
-    poly(2,:) = intrsc(1,1:2); 
-    poly(6,:) = intrsc(2,1:2);
-    iintrsc = [2,6];
-else
-    % horizontal intersection
-    if abs(intrsc(1,2) - Qymin) < tol % bottom 
-        poly(2,:) = intrsc(1,1:2);
-        iintrsc(1) = 2;
-    else % top
-        poly(6,:) = intrsc(1,1:2);
-        iintrsc(1) = 6;
     end
-    % vertical intersection
-    if abs(intrsc(2,1) - Qxmin) < tol % left 
-        poly(8,:) = intrsc(2,1:2);
-        iintrsc(2) = 8;
-    else % right
-        poly(4,:) = intrsc(2,1:2);
-        iintrsc(2) = 4;
     end
 end
 
-% swap intersection points indices
-if iintrsc(1) > iintrsc(2) 
-    tmp = iintrsc(1);
-    iintrsc(1) = iintrsc(2);
-    iintrsc(2) = tmp;
-end
 
 % insert control point
 if norm( poly(iintrsc(1),:) - controlPoints(1,:) ) < tol 
