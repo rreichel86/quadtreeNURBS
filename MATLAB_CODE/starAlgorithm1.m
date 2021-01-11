6d5ca0205cf0a4c9f9307ca04aeaf84de5bcb945
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2021-01-11 16:12:46 +0100

    refactor deleted code lines

--- MATLAB_CODE/starAlgorithm1.m
+++ MATLAB_CODE/starAlgorithm1.m
@@ -95,31 +95,11 @@ if (nPv ~= numPolyVertices)
     numPolyVertices = nPv;
 end     
 
-% insert control point
-if norm( poly(iintrsc(1),:) - controlPoints(1,:) ) < tol 
-    polygon_1 = [poly(1:iintrsc(1),:);...
-                 controlPoints(2:end-1,:);...
-                 poly(iintrsc(2):end,:)];
              
-    polygon_2 = [poly(iintrsc(1):iintrsc(2),:);...
-                 controlPoints(end-1:-1:1,:);...
-                ];
 else
-    polygon_1 = [poly(1:iintrsc(1),:);...
-                 controlPoints(end-1:-1:2,:);...
-                 poly(iintrsc(2):end,:)];
              
-    polygon_2 = [poly(iintrsc(1):iintrsc(2),:);...
-                 controlPoints(2:end,:);...
-                ];
 end
 
-polygon_1 = polygon_1(polygon_1(:,1) ~= -99,:);
-polygon_2 = polygon_2(polygon_2(:,1) ~= -99,:);
-
-% Compute polygons kernel
-K1 = computePolygonKernel(polygon_1);
-K2 = computePolygonKernel(polygon_2);
 star_shaped = ~isempty(K1) && ~isempty(K2);
 end
 
