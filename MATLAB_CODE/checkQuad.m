b1861a484bc8f77b57bb999a37a1b7dc55b6726f
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2021-01-27 17:10:38 +0100

    update checkQuad SR to handle arbitrary Quads

--- MATLAB_CODE/checkQuad.m
+++ MATLAB_CODE/checkQuad.m
@@ -1,28 +1,23 @@
-function nPoints = checkQuad( minCoords, maxCoords, Points )
-%CHECKQUAD counts the control points in given area
-%   The area is defined with the inputs minCoords and maxCoords
-%   If a control point (given with the input controlPoints) is between
-%   the maximum and minumim coordinates of the area, it can be concluded
-%   that this point is in the area, so the counter gets bigger by one for
-%   every point in the area. At the end, nPoin gives the total amount of
-%   points in the area
+function nPoints = checkQuad(polygon, points )
+% checkQuad: counts the control points in given Quad
+
 counter = 0;
 currentPoints = [];
-nPoints = size(Points,1);
+nPoints = size(points,1);
 
 
 for ii = 1:nPoints
     
-    if  isPointInQuad(minCoords, maxCoords, Points(ii,:)) == 1
+      if isPointInPolygon(polygon, points(ii,:))  ~= -1
         
         counter = counter + 1;
-        currentPoints(counter,:) = Points(ii,:);
+        currentPoints(counter,:) = points(ii,:);
     
-    end
+      end
     
 end
 
-% Check for dublicated control point 
+% check for dublicated control point 
 if ~isempty(currentPoints)
     counter = size(unique(currentPoints,'rows'),1);
 end 
