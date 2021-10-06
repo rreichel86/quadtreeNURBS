d1050b95df1d46052aaa69ee40beb03eab3931e1
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2021-10-06 10:27:46 +0200

    refactor code lines

--- MATLAB_CODE/decompose.m
+++ MATLAB_CODE/decompose.m
@@ -24,10 +24,7 @@ if ~exist('k_min','var')
 end     
 
 % Split the parent quad into 4 child quads
-[child1 , child2 , child3 , child4] = splitQuad(min(Quad(1,:)),...
-    max(Quad(1,:)), min(Quad(2,:)), max(Quad(2,:)) );
-child = [child1 , child2 , child3 , child4];
-
+[children, vertices] = splitQuad(Quad,1);
 
 for i = 1:4
     % Loop over the 4 children. First we split the eventual NURBS segment
@@ -36,18 +33,16 @@ for i = 1:4
     % those two conditions is true, we  decompose the given quad
     
     % Selecting given quad
-    Current = [child(1,2*i-1),child(1,2*i);child(2,2*i-1),child(2,2*i)];
-    Quad = [Current(1,1),Current(1,2),Current(1,2),Current(1,1);...
-            Current(2,1),Current(2,1),Current(2,2),Current(2,2)];
+    Quad = vertices(:,children(i,:));
     
-    patch(Quad(1,:),Quad(2,:),'w','FaceAlpha',0,'LineStyle','-','LineWidth',1);
+%     patch(Quad(1,:),Quad(2,:),'w','FaceAlpha',0,'LineStyle','-','LineWidth',1);
     
     %Call the splitting function. Get the tree data structure after
     %performing the splitting at the given quad and an auxiliary array
     [Q_aux, Quadtree,numInterPoints] = splitting(Quadtree,Q_aux,Quad,NURBS,l,k,i,pos_aux);
  
     %Obtains number of seed points in given quad
-    nPointsCurrent = checkQuad(Current(:,1)', Current(:,2)', NURBS.controlPoints');
+    nPointsCurrent = checkQuad(Quad', NURBS.controlPoints');
     
     %If there are more than two segments or seed points, decompose again
     if (nPointsCurrent > 1 || numInterPoints > 2 || k <= k_min)
