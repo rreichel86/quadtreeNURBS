4d5f6c93569e32086cb78bbf2b237d77d1330ebb
Le Zhang <le.zhang1@rwth-aachen.de>
2021-11-09 16:35:28 +0100

    rename this function with insertion of idxLeaf as 'extractElement'

--- MATLAB_CODE/extractElements.m
+++ MATLAB_CODE/extractElements.m
@@ -1,5 +1,5 @@
 function [numcoor,coor,numel,connectivity,maxnel,...
-          numKnotVectors,knotVectors,maxnknots,idxControlPoints] = extractElements_elevateOrder(Quadtree)
+          numKnotVectors,knotVectors,maxnknots,idxControlPoints] = extractElements(Quadtree)
 % extractElements: get polygonal elements from Quadtree data structure(with
 % insertion of idxLeaf in the connectivity matrix)
 %
