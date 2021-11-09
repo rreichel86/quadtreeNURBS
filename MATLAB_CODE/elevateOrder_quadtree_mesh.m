c7c4326e0837ad9ddb8f9e71039f88be5aa309af
Le Zhang <le.zhang1@rwth-aachen.de>
2021-11-09 22:02:05 +0100

    little changes

--- MATLAB_CODE/elevateOrder_quadtree_mesh.m
+++ MATLAB_CODE/elevateOrder_quadtree_mesh.m
@@ -66,6 +66,6 @@ hold on
                                                                     numKnotVectors,knotVectors,maxnknots,idxControlPoints);
 %% Elevate Order in P-/Q-Direction
 
-[coor,maxnsec,nnode,sections,ord]=elevtGrad(Quadtree,ep,eq,nnode,coor,sections,seedingPoints_splitt,ord,polyElmts,connectivity)
+[coor,maxnsec,nnode,sections,ord]=elevtGrad(Quadtree,ep,eq,nnode,coor,sections,seedingPoints_splitt,ord,polyElmts,connectivity);
                                                   
 end
