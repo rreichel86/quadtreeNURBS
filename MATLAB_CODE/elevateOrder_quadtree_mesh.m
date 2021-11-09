68f74a9034518f4a61519026a82b3b6e0dacb531
Le Zhang <le.zhang1@rwth-aachen.de>
2021-11-09 20:43:03 +0100

    improvement of INPUT/OUTPUT description

--- MATLAB_CODE/elevateOrder_quadtree_mesh.m
+++ MATLAB_CODE/elevateOrder_quadtree_mesh.m
@@ -25,12 +25,12 @@ function [Quadtree,nnode,coor,numsec,maxnsec,sections,ord,knots,wgt,polyElmts] =
 % sections -------------------- sectionsconnectivity matrix as nsec-tupel of 
 %                               nodes, where the first three entries
 %                               isec - section number
+%                               ipoly - polygonal element number
 %                               idxLeaf - index of Leaf
 %                               ikv - knot vector number
-%                               iel - quad element number
 %                               region - region number 
 %                               nsec - number of nodes per section
-% sections = [isec, idxLeaf, ikv, iel, region, nsec, node_1,...,node_nsec]
+% sections = [isec, ipoly, idxLeaf, ikv, region, nsec, node_1,...,node_nsec]
 % 
 % ord ------------------------- section polynomial order
 % ord = [isec, pgrad, qgrad]
