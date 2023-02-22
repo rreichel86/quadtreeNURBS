6aa303761b769a8af47b40c0cacee9560bf7b034
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2023-02-22 14:29:20 +0100

    update path to NURBS_util

--- MATLAB_CODE/config.m
+++ MATLAB_CODE/config.m
@@ -1,2 +1,2 @@
 addpath("./matlab_tree/");
-addpath("~/Desktop/fem_verbundwerkstoff/nurbs_enhanced_polygon_element/FEM/POLYTOPE_ELEMENTS/NURBS_util/");
+addpath("../../nurbs_enhanced_polygon_element/FEM/POLYTOPE_ELEMENTS/NURBS_util/");
