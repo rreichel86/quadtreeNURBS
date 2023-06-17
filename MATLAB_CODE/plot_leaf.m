19bc7225680ea5e58c56d2b5070bb431a4c5d19e
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2023-06-17 19:47:55 +0200

    include optional parameter plotOptions

--- MATLAB_CODE/plot_leaf.m
+++ MATLAB_CODE/plot_leaf.m
@@ -1,5 +1,14 @@
-function plot_leaf(Quadtree)
+function plot_leaf(Quadtree,plotOptions)
 % plot_leaf: plots each quadtree's leaf
+
+if ~exist('plotOptions','var')
+    % 1 - plot NURBS curve 
+    % 2 - plot Quad 
+    % 3 - plot control polygon and control points
+    % 4 - plot intersection points
+    plotOptions = [0 1 0 0];
+end
+
 
 % Get Quadtree leaves
 leaves = Quadtree.findleaves();
@@ -47,20 +56,29 @@ for i = 1: numLeaves
 
         % Obtain definition of the NURBS curve contained in each leaf
         NURBS = data{6};
-       
+
         ncp = length(NURBS.knots) - NURBS.degree - 1;
         NURBS_pts = CalculateNURBS(NURBS);
-        
-%         % Plot NURBS curve
-%         plot(NURBS_pts(:,1),NURBS_pts(:,2),'r','LineWidth',2);
-%         % Plot Quad
-        patch(Quad(1,:),Quad(2,:),'w','FaceAlpha',0,'LineStyle','-','LineWidth',1);
-%         % Plot control polygon
-%         plot(NURBS.controlPoints(1,:),NURBS.controlPoints(2,:),'b-.','LineWidth',1)
-%         % Plot control points
-%         plot(NURBS.controlPoints(1,2:ncp-1),NURBS.controlPoints(2,2:ncp-1),'o','Color','red','MarkerFaceColor','r','MarkerSize',6)
+
+        % Plot NURBS curve
+        if plotOptions(1) == 1
+            plot(NURBS_pts(:,1),NURBS_pts(:,2),'r','LineWidth',2);
+        end
+        % Plot Quad
+        if plotOptions(2) == 1
+            patch(Quad(1,:),Quad(2,:),'w','FaceAlpha',0,'LineStyle','-','LineWidth',1);
+        end
+        % Plot control polygon
+        if plotOptions(3) == 1
+            plot(NURBS.controlPoints(1,:),NURBS.controlPoints(2,:),'b-.','LineWidth',1)
+            % Plot control points
+%             plot(NURBS.controlPoints(1,2:ncp-1),NURBS.controlPoints(2,2:ncp-1),'o','Color','red','MarkerFaceColor','r','MarkerSize',6)
+            plot(NURBS.controlPoints(1,1:ncp),NURBS.controlPoints(2,1:ncp),'o','Color','red','MarkerFaceColor','r','MarkerSize',6)
+        end
         % Plot control points/ plot intersection points
-        plot(NURBS.controlPoints(1,1),NURBS.controlPoints(2,1),'bo','LineWidth',1.5)
+        if plotOptions(4) == 1
+            plot(NURBS.controlPoints(1,1),NURBS.controlPoints(2,1),'bo','LineWidth',1.5)
+        end
 
     end
 end
